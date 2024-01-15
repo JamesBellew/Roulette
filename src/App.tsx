824d7490dd84c2f74c354d1885e5366cb578625e
James Bellew <jamesbellew@Jamess-MacBook-Pro.local>
2024-01-15 20:09:58 +0000

    started on the chekcing for winners functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
+import './index.css'
 import './App.css'
+
 import {Random} from 'random-js'
 
 function App() {
@@ -13,14 +15,15 @@ function App() {
 
   function spinWheel(): number{
     const random = new Random();
-    const numberPicked: number  = random.integer(1,numbers.length)
+    const numberPicked: number  = random.integer(0,numbers.length)
+    setRouletteNumberLanded(numberPicked)
     console.log(numberPicked + " is the number landed");
     console.log(playerNumbersPicked + " is what you picked");
     
     // Ensure playerNumbersPicked is not undefined before calling checkForWinner
   if (playerNumbersPicked !== undefined) {
     //chekc to see if the player has won by checking its boolean return
-    if(checkForWinner(numberPicked, playerNumbersPicked)){
+    if(checkForWinnerNumber(numberPicked, playerNumbersPicked)){
 alert('you have won')
     }else{
       console.log('you lost :(');
@@ -33,7 +36,7 @@ alert('you have won')
     return numberPicked;
    
   }
-function checkForWinner(rouletteNumber:number,playerNumber:number):boolean{
+function checkForWinnerNumber(rouletteNumber:number,playerNumber:number):boolean{
 //checking to see if the 
 let flag = false;
 if (rouletteNumber === playerNumber){
@@ -43,6 +46,8 @@ flag = true
 }
 return flag
 }
+
+
 const numberSelectHandler=(userNumberPicked:number):void=>{
  setPlayerNumbersPicked(userNumberPicked)
  console.log(userNumberPicked);
@@ -52,27 +57,47 @@ const numberSelectHandler=(userNumberPicked:number):void=>{
 // spinWheel()
 return (
   <>
-    <h1>Place your bets</h1>
+    <h1 className='mb-20'>Place your bets</h1>
 
-      <p>{rouletteNumberLanded} Landed</p>
-      <p>{playerNumbersPicked} Picked</p>
-      <div className='roulette-game'>
-    <div className='roulette-table'>
-    <div className='roulette-bet-table'>
+
+      <div className='roulette-game  h-full'>
+    <div className='roulette-table w-3/5'>
+    <div className='roulette-bet-table '>
       {numbers.map(number => (
-        <button className={`${playerNumbersPicked == number ? "numberOnTableHasBet" : ""} roulette-bet-number`} onClick={()=>numberSelectHandler(number)}
+        <button className={` text-white  rounded-sm${playerNumbersPicked == number ? "numberOnTableHasBet" : ""} roulette-bet-number`} onClick={()=>numberSelectHandler(number)}
         
         key={number}>
           
           {number}
-          <div className={`${playerNumbersPicked == number ? "chipsOnNumber" : ""} `}></div>
+          <div className={`${playerNumbersPicked == number ? "chipsOnNumber" : ""} `}>
+            
+          </div>
           </button>
       ))}
     </div>
-    {}
-    <button onClick={spinWheel}>Spin</button>
+    {playerNumbersPicked != undefined &&
+    <button onClick={spinWheel} className=' btn border p- bg-slate-950 text-white px-10 py-2 rounded-md'>Spin</button>
+}
     </div>
-    <div className='roulette-wheel'></div>
+{/* <div className="roulette-wheel relative w-96 h-96 mx-auto bg-black rounded-full">
+    <div className="bg-red-600 w-[5%] mx-auto h-44 absolute left-[45%]  "></div>
+    <div className="bg-red-600 w-[5%] mx-auto h-44 absolute left-[45%] rotate-12  "></div>
+    <div className="bg-red-600 w-[5%] mx-auto h-44 absolute left-[45%]  "></div>
+</div> */}
+
+<div className='w-[40%] relative flex flex-col justify-center items-center text-center my-auto mx-auto'>
+  <div>
+    {playerNumbersPicked !== undefined &&
+      <p className='text-xl'>$50 on {playerNumbersPicked}</p>
+    }
+  </div>
+  <div>
+    {rouletteNumberLanded !== undefined &&
+      <p className='text-2xl font-bold'>{rouletteNumberLanded}</p>
+    }
+  </div>
+</div>
+
     </div>
   </>
 );
